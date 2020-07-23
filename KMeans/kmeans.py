2ffd3223cdb8f52746cad1ac760c83c922891035
Mengheng Xue <menghengxue@gmail.com>
2020-07-23 11:31:02 -0400

    update kmeans

--- KMeans/kmeans.py
+++ KMeans/kmeans.py
@@ -9,7 +9,39 @@ def euclidean_distance(x1, x2):
 
 class KMeans:
 
-    def __init__(self):
+    def __init__(self, K=5, max_iters=100, plot_steps=False):
+        self.K = K
+        self.max_iters = max_iters
+        self.plot_steps = plot_steps
+
+        # list of sample indices for each cluster
+        self.clusters = [[] for _ in range(self.K)]
+
+        # the centers (mean feature vector) for each cluster
+        self.centroids = []
+
+    def predict(self, X):
+        self.X = X
+        self.n_samples, self.n_features = X.shape
+
+        # initialize
+        random_sample_idxs = np.random.choice(self.n_samples, self.K, replace=False)
+
+        # Optimize clusters
+        for _ in range(self.max_iters):
+            # Assign samples to closet centroids (create clusters)
+            self.clusters = self._create_clusters(self.centroids)
+
+
+    def _creat_clusters(self, centroids):
+        # Assign the samples to the closest centroids to  create clusters
+        clusters = [[] for _ in range(self.K)]
+
+    def _closest_centroid(self, sample, centroids):
         pass
 
+    def _get_cluster_labels(self, clusters):
+        pass
 
+    def _create_clusters(self, centroids):
+        pass
